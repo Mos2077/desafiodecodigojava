8a8e2883ea64aaa6edd81aef287956f527bc2e7c
Matheus O Silva <78383751+Mos2077@users.noreply.github.com>
2024-07-26 00:53:20 -0300

    Update Desafio1.java

--- Desafio1.java
+++ Desafio1.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 // Implementação da Interface, EvaluationStrategy e seu método abstrato:
 interface EvaluationStrategy {
     String evaluate(double average);
 }
+
 
 // Classe Concreta, ExcellentStrategy e sua lógica de resolução aplicada:
 class ExcellentStrategy implements EvaluationStrategy {
@@ -14,41 +16,54 @@ class ExcellentStrategy implements EvaluationStrategy {
     }
 }
 
-// Classe Concreta, GoodStrategy e sua lógica de avaliação aplicada:
+
+// TODO: Crie a classe Concreta, GoodStrategy e sua lógica de avaliação aplicada:
 class GoodStrategy implements EvaluationStrategy {
     public String evaluate(double average) {
         return average >= 7 && average < 9 ? "Bom" : "";
     }
 }
 
-// Classe Concreta, RegularStrategy e sua lógica de avaliação aplicada:
+
+// TODO: Crie a classe Concreta, RegularStrategy e sua lógica de avaliação aplicada:
 class RegularStrategy implements EvaluationStrategy {
     public String evaluate(double average) {
         return average >= 5 && average < 7 ? "Regular" : "";
     }
 }
 
-// Classe Concreta, UnsatisfactoryStrategy e sua lógica de avaliação aplicada:
+
+// TODO: Crie a classe Concreta, UnsatisfactoryStrategy e sua lógica de avaliação aplicada:
 class UnsatisfactoryStrategy implements EvaluationStrategy {
     public String evaluate(double average) {
-        return average < 5 ? "Insatisfatório" : "";
+        return average < 5 ? "Insatisfatorio" : "";
     }
 }
 
+
 public class ProjectEvaluation {
 
+
     private static final List<Double> evaluations = new ArrayList<>();
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+
+
         double evaluation1 = scanner.nextDouble();
         evaluations.add(evaluation1);
 
+
+  
         double evaluation2 = scanner.nextDouble();
         evaluations.add(evaluation2);
-
+        
+        scanner.close();  
         double average = calculateAverage(evaluations);
+
 
         EvaluationStrategy[] strategies = {
             new ExcellentStrategy(),
@@ -56,6 +71,7 @@ public class ProjectEvaluation {
             new RegularStrategy(),
             new UnsatisfactoryStrategy()
         };
+
 
         String status = "";
         for (EvaluationStrategy strategy : strategies) {
@@ -65,8 +81,10 @@ public class ProjectEvaluation {
             }
         }
 
+
         System.out.printf("Media: %.1f. Status: %s.%n", average, status);
     }
+
 
     private static double calculateAverage(List<Double> evaluations) {
         double sum = 0;
